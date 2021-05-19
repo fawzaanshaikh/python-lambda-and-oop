605c92b642dcfd7756bc2ac3cc08b4589c598919
Fawzaan <fawzi20112001@gmail.com>
2021-05-20 01:43:26 +0530

    Added lambda.py

---
+++ lambda.py
@@ -0,0 +1,20 @@
+""" Python Code for Lambda Functions Explanations and Examples """
+
+# Lambda Functions are one-liner functions that do not have a name and return a result 
+
+# functionName = lambda variable_name: operation on variable_name
+
+addTen = lambda x: x + 10
+print(addTen(29))
+
+# square = lambda x: x * x
+# print(square(8))
+
+# add = lambda x, y: x + y
+# print(add(2, 3))
+
+# moreNumbersAdd = lambda x, y, z: x + y + z
+# print(moreNumbersAdd(1, 2, 3))
+
+# constantSubtract = lambda x: 1 - 2
+# print(constantSubtract(4))
